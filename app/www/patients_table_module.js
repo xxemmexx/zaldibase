1c56560faf359f71893297b9d32255fc6e6bf7b9
amas <amas_84@krutt.org>
2022-05-05 14:48:08 +0200

    added folder www

---
+++ app/www/patients_table_module.js
@@ -0,0 +1,16 @@
+
+
+
+function patients_table_module_js(ns_prefix) {
+
+  $("#" + ns_prefix + "patients_table").on("click", ".delete_btn", function() {
+    Shiny.setInputValue(ns_prefix + "patient_id_to_delete", this.id, { priority: "event"});
+    $(this).tooltip('hide');
+  });
+
+  $("#" + ns_prefix + "patients_table").on("click", ".edit_btn", function() {
+    Shiny.setInputValue(ns_prefix + "patient_id_to_edit", this.id, { priority: "event"});
+    $(this).tooltip('hide');
+  });
+}
+
