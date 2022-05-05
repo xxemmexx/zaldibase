018a18ec04c855b89980e8c41357bca68243fe9c
amas <amas_84@krutt.org>
2022-05-05 11:21:07 +0200

    Cleaned up file create_model_data

--- data/create_model_data.R
+++ data/create_model_data.R
@@ -1,13 +1,8 @@
 library(dplyr)
-#library(tidyr)
 require(tibble)
-
-
 
 patients <- tribble(~Nom, ~Prénom, ~`Date de naissance`, ~Condition, ~`Hôpital d'origine`, ~`Personne de contact`,  
                     'Vera', 'Daniel', '1985-12-29', 'Le duele la verga', 'Centre Hospitalier Albertville-Moûtiers', 'Albert Malfait',
                     'Verdu', 'Anaïs', '1985-02-20', 'Le huele la cola', 'Centre Hospitalier Albertville-Moûtiers', 'Albert Malfait')
 
-
-
-saveRDS(patients, file = '/data/patients_dummy.RDS')
+saveRDS(patients, file = 'data/patients_dummy.RDS')
