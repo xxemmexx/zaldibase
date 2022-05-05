d388217c766f77c8425baf8eaf21e619552944fa
amas <amas_84@krutt.org>
2022-05-05 11:20:22 +0200

    Created dummy file for patients

--- data/create_model_data.R
+++ data/create_model_data.R
@@ -2,8 +2,6 @@ library(dplyr)
 #library(tidyr)
 require(tibble)
 
-# attach data frame from 'mtcars' dataset to global environment
-mtcars <- datasets::mtcars
 
 
 patients <- tribble(~Nom, ~Prénom, ~`Date de naissance`, ~Condition, ~`Hôpital d'origine`, ~`Personne de contact`,  
@@ -11,20 +9,5 @@ patients <- tribble(~Nom, ~Prénom, ~`Date de naissance`, ~Condition, ~`Hôpital
                     'Verdu', 'Anaïs', '1985-02-20', 'Le huele la cola', 'Centre Hospitalier Albertville-Moûtiers', 'Albert Malfait')
 
 
-# convert the rownames to an actual column.  We suggest to never use row names, just
-# use a regular column.
-mtcars <- rownames_to_column(mtcars, var = 'model')
 
-# Converting Weight (i.e. 'wt') from 1000's of lbs to lbs
-mtcars$wt <- mtcars$wt * 1000
-
-
-# Converting binary values to intended, character values
-mtcars <- mtcars %>%
-  mutate(
-    vs = ifelse(vs == 0, 'V-shaped', 'Straight'),
-    am = ifelse(am == 0, 'Automatic', 'Manual')
-  )
-
-
-saveRDS(mtcars, file = '01_traditional/data_prep/prepped/mtcars.RDS')
+saveRDS(patients, file = '/data/patients_dummy.RDS')
